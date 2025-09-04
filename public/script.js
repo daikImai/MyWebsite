4aadc879171c4823d3b86b71c9905141747b1879
daikiImai <8ikiad@gmail.com>
2025-09-04 23:23:06 +0900

    added scroll

--- public/script.js
+++ public/script.js
@@ -1,14 +1,13 @@
 "use strict";
 
-const title = document.querySelector('#title');
 const keyframes = {
     opacity: [0, 1],
 };
+
 const options = {
     duration: 3000,
     easing: 'ease',
 }
-title.animate(keyframes, options);
 
 const animateFade = (entries, obs) => {
     entries.forEach((entry) => {
@@ -27,8 +26,25 @@ const animateFade = (entries, obs) => {
         }
     });
 };
+
 const fadeObserver = new IntersectionObserver(animateFade);
+
 const fadeElements = document.querySelectorAll('.fadein');
 fadeElements.forEach((fadeElement) => {
     fadeObserver.observe(fadeElement);
 });
+
+function scrollToAbout(e) {
+    e.preventDefault(); // #about をURLにつけない
+    document.getElementById("about").scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToMyWorks(e) {
+    e.preventDefault(); 
+    document.getElementById("my-works").scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToContact(e) {
+    e.preventDefault(); 
+    document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+}
